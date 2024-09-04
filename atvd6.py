e2e7022a9619dbca5ed090b2f6df90d154e666f3
mairoklpss <mairalopesma@gmail.com>
2024-09-04 15:50:43 -0300

    nome + sobrenome

---
+++ atvd6.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+nome1 = input("escreva seu primeiro nome: ")
+nome2 = input("escreva seu sobrenome: ")
+
+print(f"Olá, {nome1} {nome2}!")
